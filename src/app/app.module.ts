d990cd2efb3fd829dddd9b29c89faef61c229ab3
¨a5r8c18¨ <cobaseliu@gmail.com>
2024-10-15 19:34:51 -0400

    tercer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app.routes';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { TareaComponent } from './tarea/tarea.component';
 
+
 @NgModule({
 declarations: [
 AppComponent,
@@ -31,7 +32,7 @@ MatIconModule,
 MatToolbarModule,
 MatSidenavModule,
 MatListModule,
-AppRoutingModule
+AppRoutingModule,
 ],
 providers: [],
 bootstrap: [AppComponent]
